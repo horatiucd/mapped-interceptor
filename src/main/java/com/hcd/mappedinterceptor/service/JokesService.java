49c468a8349c7f796d4c297cbb6f68006d0f3342
horatiu.dan <horatiu.dan@tangoe.com>
2022-09-07 10:45:23 +0300

    Correct a typo.

--- src/main/java/com/hcd/mappedinterceptor/service/JokesService.java
+++ src/main/java/com/hcd/mappedinterceptor/service/JokesService.java
@@ -13,7 +13,7 @@ public class JokesService {
     public JokesService() {
         jokes.addAll(List.of(new Joke("If Chuck Norris coughs on you, you owe him 50 bucks."),
                 new Joke("Chuck Norris can make a slinky go up the stairs."),
-                new Joke("Ice has Chuck Norris running through its veins")));
+                new Joke("Ice has Chuck Norris running through its veins.")));
     }
 
     public List<Joke> getJokes() {
